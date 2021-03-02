11cacb1676e25b0f9a1951558274d85b5b571a32
Kroh Nickolay <375336623761@ya.ru>
2021-03-02 21:24:10 +0300

    refactor: extract controller from Board

--- client/src/components/Board/Board.jsx
+++ client/src/components/Board/Board.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import Cell from '../Cell'
-import {updateCells, showEndWindow, decrementBombLeft, incrementBombLeft, newGame, setGameStarted } from '../../redux/actions'
-import { REVEAL, BOMB, FLAG, BTN } from '../../constants/cell_types';
+import {updateCells, newGame} from '../../redux/actions';
+import {
+  cellClickHandler, 
+  markCell, 
+  revealAroundCell,
+  clickHandler,
+  bothClickHandler,
+  rightClickHandler,
+  createCells
+} from './controller.Board';
 import './Board.scss';
- 
+
 const Board = () => {
   const dispatch = useDispatch();
   const cellsData = useSelector(state => state.board.cells);
@@ -15,234 +23,26 @@ const Board = () => {
   const lastY = colLength - 1;
   const lastX = rowLength - 1;
 
-  const createCells = (bombs, lastY, lastX) => {
-    const cells = new Array(colLength)
-      .fill(undefined)
-      .map(() => new Array(rowLength)
-        .fill(undefined)
-        .map(() => ({type: BTN, value: 0})));
+  const cellClickCallback = (x, y) => cellClickHandler(
+    x, y, cellsData, started, bombs, lastY, lastX, dispatch
+  );
 
-    let unplacedBombs = bombs;
+  const markCallback = (x, y) => markCell(x, y, cellsData, dispatch);
 
-    while (unplacedBombs) {
-      const x = Math.floor(Math.random() * lastX);
-      const y = Math.floor(Math.random() * lastY);
-
-      if (cells[y][x].value === BOMB) continue;
-      
-      cells[y][x].value = BOMB;
-      unplacedBombs -= 1;
-
-      if (y > 0 && cells[y - 1][x].value !== BOMB)
-        cells[y - 1][x].value += 1;
-      if (y < colLength - 1 && cells[y + 1][x].value !== BOMB)
-        cells[y + 1][x].value += 1;
-      if (x > 0 && cells[y][x - 1].value !== BOMB)
-        cells[y][x - 1].value += 1;
-      if (x < rowLength - 1 && cells[y][x + 1].value !== BOMB)
-        cells[y][x + 1].value += 1;
-      
-      if (y > 0 && x > 0 && cells[y - 1][x - 1].value !== BOMB)
-        cells[y - 1][x - 1].value += 1;
-      if (y > 0 && x < rowLength - 1 && cells[y - 1][x + 1].value !== BOMB)
-        cells[y - 1][x + 1].value += 1;
-      if (y < colLength - 1 && x > 0 && cells[y + 1][x - 1].value !== BOMB)
-        cells[y + 1][x - 1].value += 1;
-      if (
-        y < colLength - 1 && 
-        x < rowLength - 1 && 
-        cells[y + 1][x + 1].value !== BOMB
-      )
-        cells[y + 1][x + 1].value += 1;
-    }
-
-    return cells;
-  }
-
-  const revealCellsArea = (startX, startY, cells) => {
-    const queue = [{x: startX, y: startY}];
-  
-    while(queue.length) {
-      const {x, y} = queue.pop();
-      const current = cells[y][x];
-  
-      current.type = REVEAL;
-  
-      if (current.value !== 0) continue;
-  
-      const top = y > 0 && cells[y - 1][x];
-      const bottom = y < lastY && cells[y + 1][x];
-      const left = x > 0 && cells[y][x - 1];
-      const right = x < lastX && cells[y][x + 1];
-      const topLeft = y > 0 && x > 0 && cells[y - 1][x - 1];
-      const topRight = y > 0 && x < lastX && cells[y - 1][x + 1];
-      const bottomLeft = y < lastY && x > 0 && cells[y + 1][x - 1];
-      const bottomRight = y < lastY && x < lastX && cells[y + 1][x + 1];
-  
-      if (top && top.type !== REVEAL && top.value !== BOMB)
-        queue.push({x, y: y - 1});
-      if (bottom && bottom.type !== REVEAL && bottom.value !== BOMB)
-        queue.push({x, y: y + 1});
-      if (left && left.type !== REVEAL && left.value !== BOMB)
-        queue.push({x: x - 1, y});
-      if (right && right.type !== REVEAL && right.value !== BOMB)
-        queue.push({x: x + 1, y});
-      if (topLeft && topLeft.type !== REVEAL && topLeft.value !== BOMB)
-        queue.push({x: x - 1, y: y - 1});
-      if (topRight && topRight.type !== REVEAL && topRight.value !== BOMB)
-        queue.push({x: x + 1, y: y - 1});
-      if (bottomLeft && bottomLeft.type !== REVEAL && bottomLeft.value !== BOMB)
-        queue.push({x: x - 1, y: y + 1});
-      if (bottomRight && bottomRight.type !== REVEAL && bottomRight.value !== BOMB)
-        queue.push({x: x + 1, y: y + 1});
-    }
-  }
-  
-  const checkGameWin = (cells) => {
-    const isGameEnd = cells.every(row => row.every(cell => {
-      if (cell.value !== BOMB && cell.type !== REVEAL)
-        return false;
-
-      return true;
-      })
-    )
-
-    if (isGameEnd) dispatch(showEndWindow('You win!'));
-  }
-
-  const hitAreaByFirstClick = (x, y, cells) => {
-    let cellsCopy = cells;
-
-    while (cellsCopy[y][x].value !== 0) {
-      cellsCopy = createCells(bombs, lastY, lastX);
-    }
-
-    return cellsCopy;
-  }
-
-  const firstClickHandler = (x, y, cells) => {
-    dispatch(setGameStarted(true));
-    return hitAreaByFirstClick(x, y, cells);
-  }
-
-  const revealCells = (x, y, cells) => {
-    cells[y][x].type = REVEAL;
-
-    switch (cells[y][x].value) {
-      case 0:
-        revealCellsArea(x, y, cells);
-        break;
-      case BOMB:
-        dispatch(updateCells(cells));
-        dispatch(showEndWindow('you lose'));
-        return;
-      default:
-        break;
-    }
-
-    checkGameWin(cells);
-  }
-
-  const revealAroundCell = (startX, startY, cells) => {
-    const bombsAround = cells[startY][startX].value;
-    const top = startY > 0 && {y:startY - 1, x:startX};
-    const bottom = startY < lastY && {y:startY + 1,x:startX};
-    const left = startX > 0 && {y:startY, x:startX - 1};
-    const right = startX < lastX && {y:startY, x:startX + 1};
-    const topLeft = startY > 0 && startX > 0 && {y:startY - 1, x:startX - 1};
-    const topRight = startY > 0 && startX < lastX && {y:startY - 1, x:startX + 1};
-    const bottomLeft = 
-      startY < lastY && 
-      startX > 0 && 
-      {y:startY + 1, x:startX - 1};
-    const bottomRight = 
-      startY < lastY && 
-      startX < lastX && 
-      {y:startY + 1, x:startX + 1};
-    const surroundingCells = [
-      top, bottom, left, right, topLeft, topRight, bottomLeft, bottomRight
-    ];
-
-    const bombsMarked = surroundingCells.reduce((marks, coord) => {
-      if (coord && cells[coord.y][coord.x].type === FLAG)
-        return marks + 1;
-      
-      return marks;
-    }, 0);
-
-    if (bombsMarked !== bombsAround) return;
-
-    surroundingCells.forEach(coord => {
-      if (coord && cells[coord.y][coord.x].type === BTN)
-        revealCells(coord.x, coord.y, cells);
-    });
-
-    dispatch(updateCells(cells));
-  }
-  
-  const clickHandler = (event) => {
-    const cell = event.target.closest('.cell');
-    
-    if (!cell) return;
-    
-    const x = Number(cell.dataset.coordX);
-    const y = Number(cell.dataset.coordY);
-
-    if (cellsData[y][x].type !== BTN) return;
-    
-    let cellsDataCopy = [...cellsData];
-
-    if (!started)
-      cellsDataCopy = firstClickHandler(x, y, cellsDataCopy);
-
-    revealCells(x, y, cellsDataCopy);
-    dispatch(updateCells(cellsDataCopy));
+  const revealAroundCallback = (x, y) => {
+    revealAroundCell(x, y, cellsData, lastX, lastY, dispatch);
   }
 
   const mouseUpHandler = (event) => {
     if (event.nativeEvent.which === 1 && event.buttons === 0)
-      return clickHandler(event);
-
-    if (event.nativeEvent.which !== 3) return;
-
-    const cell = event.target.closest('.cell');
-    
-    if (!cell) return;
-    
-    const x = Number(cell.dataset.coordX);
-    const y = Number(cell.dataset.coordY);
-    const cellType = cellsData[y][x].type;
-
-    if (cellType !== BTN && cellType !== FLAG) return;
-    
-    const cellsDataCopy = [...cellsData];
-
-    if (cellType === BTN) {
-      cellsDataCopy[y][x].type = FLAG;
-      dispatch(decrementBombLeft());
-    } else {
-      cellsDataCopy[y][x].type = BTN;
-      dispatch(incrementBombLeft());
-    }
-
-    dispatch(updateCells(cellsDataCopy));
+      clickHandler(event, cellClickCallback);
+    else if (event.nativeEvent.which === 3)
+      rightClickHandler(event, markCallback);
   }
 
   const mouseDownHandler = (event) => {
-    if (event.buttons !== 3) return;
-
-    const cell = event.target.closest('.cell');
-    
-    if (!cell) return;
-    
-    const x = Number(cell.dataset.coordX);
-    const y = Number(cell.dataset.coordY);
-
-    if (cellsData[y][x].type !== REVEAL) return;
-
-    const cellsDataCopy = [...cellsData];
-
-    revealAroundCell(x, y, cellsDataCopy);
+    if (event.buttons === 3)
+      bothClickHandler(event, revealAroundCallback);
   }
 
   const cells = cellsData.map((row, y) => row.map((cell, x) => (
---
+++ client/src/components/Board/controller.Board.js
@@ -0,0 +1,216 @@
+import { REVEAL, BOMB, FLAG, BTN } from '../../constants/cell_types';
+import {
+  updateCells,
+  showEndWindow,
+  decrementBombLeft,
+  incrementBombLeft,
+  setGameStarted,
+} from '../../redux/actions';
+
+const getCoordCellsAround = (x, y, cells, lastX, lastY) => {
+  const top = y > 0 && { y: y - 1, x: x };
+  const bottom = y < lastY && { y: y + 1, x: x };
+  const left = x > 0 && { y: y, x: x - 1 };
+  const right = x < lastX && { y: y, x: x + 1 };
+  const topLeft = y > 0 && x > 0 && { y: y - 1, x: x - 1 };
+  const topRight = y > 0 && x < lastX && { y: y - 1, x: x + 1 };
+  const bottomLeft = y < lastY && x > 0 && { y: y + 1, x: x - 1 };
+  const bottomRight = y < lastY && x < lastX && { y: y + 1, x: x + 1 };
+
+  const cellsAround = [
+    top,
+    bottom,
+    left,
+    right,
+    topLeft,
+    topRight,
+    bottomLeft,
+    bottomRight,
+  ];
+
+  return cellsAround.filter((obj) => obj);
+};
+
+export const createCells = (bombs, lastY, lastX) => {
+  const cells = new Array(lastY + 1)
+    .fill(undefined)
+    .map(() =>
+      new Array(lastX + 1).fill(undefined).map(() => ({ type: BTN, value: 0 }))
+    );
+
+  let unplacedBombs = bombs;
+
+  while (unplacedBombs) {
+    const bombX = Math.floor(Math.random() * lastX);
+    const bombY = Math.floor(Math.random() * lastY);
+
+    if (cells[bombY][bombX].value === BOMB) continue;
+
+    cells[bombY][bombX].value = BOMB;
+    unplacedBombs -= 1;
+
+    const cellsAround = getCoordCellsAround(bombX, bombY, cells, lastX, lastY);
+
+    cellsAround.forEach(({ x, y }) => {
+      if (cells[y][x].value !== BOMB) cells[y][x].value += 1;
+    });
+  }
+
+  return cells;
+};
+
+const revealCellsArea = (startX, startY, cells, lastX, lastY) => {
+  const queue = [{ x: startX, y: startY }];
+
+  while (queue.length) {
+    const { x, y } = queue.pop();
+    const current = cells[y][x];
+
+    current.type = REVEAL;
+
+    if (current.value !== 0) continue;
+
+    const cellsAround = getCoordCellsAround(x, y, cells, lastX, lastY);
+
+    cellsAround.forEach((coord) => {
+      if (
+        cells[coord.y][coord.x].type !== REVEAL &&
+        cells[coord.y][coord.x].value !== BOMB
+      ) {
+        queue.push(coord);
+      }
+    });
+  }
+};
+
+const checkGameWin = (cells, dispatch) => {
+  const isGameEnd = cells.every((row) =>
+    row.every((cell) => {
+      if (cell.value !== BOMB && cell.type !== REVEAL) return false;
+
+      return true;
+    })
+  );
+
+  if (isGameEnd) dispatch(showEndWindow('You win!'));
+};
+
+const revealCell = (x, y, cells, lastX, lastY, dispatch) => {
+  cells[y][x].type = REVEAL;
+
+  switch (cells[y][x].value) {
+    case 0:
+      revealCellsArea(x, y, cells, lastX, lastY);
+      break;
+    case BOMB:
+      dispatch(updateCells(cells));
+      dispatch(showEndWindow('you lose'));
+      return;
+    default:
+      break;
+  }
+
+  checkGameWin(cells, dispatch);
+};
+
+export const revealAroundCell = (x, y, cells, lastX, lastY, dispatch) => {
+  if (cells[y][x].type !== REVEAL) return;
+
+  const cellsCopy = [...cells];
+  const bombsAround = cellsCopy[y][x].value;
+  const surroundingCells = getCoordCellsAround(x, y, cellsCopy, lastX, lastY);
+
+  const bombsMarked = surroundingCells.reduce((marks, coord) => {
+    if (cellsCopy[coord.y][coord.x].type === FLAG) return marks + 1;
+
+    return marks;
+  }, 0);
+
+  if (bombsMarked !== bombsAround) return;
+
+  surroundingCells.forEach((coord) => {
+    if (cellsCopy[coord.y][coord.x].type === BTN)
+      revealCell(coord.x, coord.y, cellsCopy, lastX, lastY, dispatch);
+  });
+
+  dispatch(updateCells(cellsCopy));
+};
+
+const hitAreaByFirstClick = (x, y, cells, bombs, lastY, lastX) => {
+  let cellsCopy = cells;
+
+  while (cellsCopy[y][x].value !== 0) {
+    cellsCopy = createCells(bombs, lastY, lastX);
+  }
+
+  return cellsCopy;
+};
+
+const firstClickHandler = (x, y, cells, bombs, lastY, lastX, dispatch) => {
+  dispatch(setGameStarted(true));
+  return hitAreaByFirstClick(x, y, cells, bombs, lastY, lastX);
+};
+
+const runCbIfCell = (element, callback) => {
+  const cell = element.closest('.cell');
+
+  if (!cell) return;
+
+  const x = Number(cell.dataset.coordX);
+  const y = Number(cell.dataset.coordY);
+
+  callback(x, y);
+};
+
+export const clickHandler = (event, callback) =>
+  runCbIfCell(event.target, callback);
+export const bothClickHandler = clickHandler;
+export const rightClickHandler = clickHandler;
+
+export const cellClickHandler = (
+  x,
+  y,
+  cellsData,
+  started,
+  bombs,
+  lastY,
+  lastX,
+  dispatch
+) => {
+  if (cellsData[y][x].type !== BTN) return;
+
+  let cellsDataCopy = [...cellsData];
+
+  if (!started) {
+    cellsDataCopy = firstClickHandler(
+      x,
+      y,
+      cellsDataCopy,
+      bombs,
+      lastY,
+      lastX,
+      dispatch
+    );
+  }
+
+  revealCell(x, y, cellsDataCopy, lastX, lastY, dispatch);
+  dispatch(updateCells(cellsDataCopy));
+};
+
+export const markCell = (x, y, cellsData, dispatch) => {
+  const cellType = cellsData[y][x].type;
+
+  if (cellType !== BTN && cellType !== FLAG) return;
+
+  const cellsDataCopy = [...cellsData];
+
+  if (cellType === BTN) {
+    cellsDataCopy[y][x].type = FLAG;
+    dispatch(decrementBombLeft());
+  } else {
+    cellsDataCopy[y][x].type = BTN;
+    dispatch(incrementBombLeft());
+  }
+
+  dispatch(updateCells(cellsDataCopy));
+};
